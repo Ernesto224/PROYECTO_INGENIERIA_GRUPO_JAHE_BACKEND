e33ae6e99051e1f402c3f121dc4276d3d26d01a4
HarveyMendez <harvey.mendez@ucr.ac.cr>
2025-03-30 14:45:45 -0600

    Agregado funcionalidad sobre publicidad

---
+++ 2025_GRUPO_JAHE_BACKEND/Dominio/Entidades/Publicidad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    [Table("Publicidad")]
+    public class Publicidad
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPublicidad { get; set; }
+
+        [Required]
+        public string EnlacePublicidad { get; set; } = string.Empty;
+
+        [Required]
+        public bool Activo { get; set; }
+
+        // Clave foránea
+        [ForeignKey("Imagen")]
+        public int IdImagen { get; set; }
+        public Imagen? Imagen { get; set; }
+    }
+}
