21f13d7dffed91bbbcc5e1c8dc67c16fe0d7999b
HarveyMendez <harvey.mendez@ucr.ac.cr>
2025-06-26 09:38:33 -0600

    agregado manejo de temporada alta

---
+++ 2025_GRUPO_JAHE_BACKEND/Aplicacion/DTOs/TemporadaDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.DTOs
+{
+    public class TemporadaDTO
+    {
+        public int IdTemporada { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime FechaFinal { get; set; }
+
+        public int Porcentaje { get; set; }
+    }
+}
--- 2025_GRUPO_JAHE_BACKEND/Infraestructura/Repositorios/TemporadaRepositorio.cs
+++ 2025_GRUPO_JAHE_BACKEND/Infraestructura/Repositorios/TemporadaRepositorio.cs
@@ -5,16 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using Dominio.Entidades;
 using Dominio.Interfaces;
+using Infraestructura.Nucleo;
 using Infraestructura.Persistencia;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infraestructura.Repositorios
 {
-    public class TemporadaRepositorio : ITemporadaRepositorio
+    public class TemporadaRepositorio : BaseRepositorio<Temporada>, ITemporadaRepositorio
     {
         private readonly ContextoDbSQLServer _context;
 
-        public TemporadaRepositorio(ContextoDbSQLServer context)
+        public TemporadaRepositorio(ContextoDbSQLServer context) : base(context)
         {
             _context = context;
         }
@@ -32,5 +33,29 @@ namespace Infraestructura.Repositorios
             return temporada;
         }
 
-    }
+        public async Task<Temporada> ObtenerTemporadaAlta()
+        {
+            var temporada = await _context.Temporadas
+                .FirstOrDefaultAsync(t => t.Nombre == "Alta Temporada");
+
+            if (temporada == null)
+            {
+                return null;
+            }
+
+            return temporada;
+        }
+
+        public async Task<Temporada> ModificarTemporada(Temporada temporada)
+        {
+            if (temporada == null) throw new ArgumentNullException(nameof(temporada));
+            _context.Temporadas.Update(temporada);
+            await _context.SaveChangesAsync();
+            return temporada;
+        }
+
+
+
+
+        }
 }
