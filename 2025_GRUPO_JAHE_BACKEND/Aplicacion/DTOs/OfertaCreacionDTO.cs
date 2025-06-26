31888c46b9cd5d54a616a1724d1c178800e31835
HarveyMendez <harvey.mendez@ucr.ac.cr>
2025-06-26 08:41:33 -0600

    completada la US23 agregar oferta

---
+++ 2025_GRUPO_JAHE_BACKEND/Aplicacion/DTOs/OfertaCreacionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.DTOs
+{
+    public class OfertaCreacionDTO
+    {
+        public OfertaDTO ofertaDTO { get; set; }
+        public byte[]? Imagen { get; set; }
+        public string NombreArchivo { get; set; } = string.Empty;
+    }
+}
