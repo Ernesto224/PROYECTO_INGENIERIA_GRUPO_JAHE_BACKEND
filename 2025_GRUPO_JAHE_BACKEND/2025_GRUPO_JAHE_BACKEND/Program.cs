8e6c109622bf1785bd2639f34ffa274258d54371
Ernesto224 <ev402648@gmail.com>
2025-06-26 23:16:10 -0600

    modificacion para hosting

--- 2025_GRUPO_JAHE_BACKEND/2025_GRUPO_JAHE_BACKEND/Program.cs
+++ 2025_GRUPO_JAHE_BACKEND/2025_GRUPO_JAHE_BACKEND/Program.cs
@@ -111,11 +111,11 @@ app.UseCors(options =>
 });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
